69f99e655598dc4a3d7f04e292e70b4657ca7dfd
KeyboardFire <andy@keyboardfire.com>
2018-11-20 00:16:05 -0600

    Add color to dungeon and player sections
    This also involved adding some section headers and column padding,
    because the whole thing was starting to get a little cramped.

--- gen.rb
+++ gen.rb
@@ -1,24 +1,27 @@
 #!/usr/bin/ruby
 
-def tr a
-    ([nil]*a.map(&:size).max).zip(*a).map &:compact
+def tr a, c=true
+    transposed = ([nil]*a.map(&:size).max).zip(*a)
+    c ? transposed.map(&:compact) : transposed.map{|x| x[1..-1]}
 end
 
 data = Dir['data/*/*.txt'].map{|x| [x.split(?/)[-1][0..-5], File.read(x)]}.to_h
-spec = File.readlines('data/spec').map{|x| x.split(nil, 2)}.to_h
+spec = File.readlines('data/spec').map{|x| x.split(nil, 2)}.to_h.transform_values{|v|
+    v.split.map{|x| w, d = x.scan(/^\d+|.+/); [w.to_i, d]}
+}
 layout = tr File.read('data/layout-3col').lines.map(&:split)
 colorscheme = '
         .bk, .C                         { color: #555; }
         .re, .DRA                       { color: #a00; }
-        .gr, .VEG                       { color: #0a0; }
+        .gr, .VEG, .E                   { color: #0a0; }
         .br, .FLE, .LTH, .WOO           { color: #a50; }
         .bl                             { color: #00a; }
-        .ma                             { color: #a0a; }
+        .ma, .S                         { color: #a0a; }
         .cy, .IRN, .MTH, .MET           { color: #0aa; }
-        .gy, .N, .SLV, .MIN             { color: #aaa; }
+        .gy, .N, .SLV, .MIN, .B         { color: #aaa; }
         .no                             { color: #ddd; }
         .or                             { color: #f55; }
-        .bg                             { color: #5f5; }
+        .bg, .M, .GM                    { color: #5f5; }
         .ye, .CPR, .GLD                 { color: #ff5; }
         .bb                             { color: #55f; }
         .bm, .CLO, .PLS, .GEM, .MAT     { color: #f5f; }
@@ -54,9 +57,10 @@ File.open('cnrs.html', ?w) do |f|
 
     cols = layout.map do |col|
         col.map do |sec|
-            rows = []
-            sp = spec[sec].split.map{|x| w, d = x.scan(/^\d+|.+/); [w.to_i, d]}
-            data[sec].each_line do |line|
+            rows = [html("##### #{sec} #####".center(80), 'bb'), ' ' * 80]
+            sp = spec[sec]
+            data[sec].each_line.with_index do |line, nl|
+                sp = spec["#{sec}#{nl}"] || sp
                 line.chomp!
                 len = 0
                 if line[0] == ?=
@@ -69,7 +73,7 @@ File.open('cnrs.html', ?w) do |f|
                     sp.each do |w, d|
                         clr = case d
                             when /^\$(\d*)$/ then eol[$1.to_i,2]
-                            when ?^ then line[idx,w]
+                            when ?^ then (line[idx,w] || '').strip
                             else d.clone
                             end
                         if clr && clr[0] == ?<
@@ -79,7 +83,7 @@ File.open('cnrs.html', ?w) do |f|
                             rows[-1] += ' '
                             len += 1
                         end
-                        while clr && clr[-3] =~ /[*!]/
+                        while clr && clr[-3] =~ /[^a-z0-9$^]/
                             clr = eol.include?(clr[-3]) ? clr[-2,2] : clr[0...-3]
                         end
                         rows[-1] += html(line[idx,w], clr)
@@ -93,7 +97,7 @@ File.open('cnrs.html', ?w) do |f|
         end.reduce{|a,x| a + [' '*80] + x }
     end
 
-    f.puts tr(cols).map(&:join)
+    f.puts tr(cols, false).map{|x| x.map{|y| y ? y : ' '*80}.join '  ' }
 
     f.puts <<~X
             </pre>
