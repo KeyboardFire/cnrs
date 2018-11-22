9ede545f6c95a80091e2710765b5d0b80de98f47
KeyboardFire <andy@keyboardfire.com>
2018-11-21 21:07:34 -0600

    Begin generalizing layout/font/colors
    The goal is to generate many combinations so that each user can choose
    their personal preference.

--- gen.rb
+++ gen.rb
@@ -1,12 +1,8 @@
 #!/usr/bin/ruby
 
-def tr a, c=true
-    transposed = ([nil]*a.map(&:size).max).zip(*a)
-    c ? transposed.map(&:compact) : transposed.map{|x| x[1..-1]}
-end
-
-@data = Dir['data/*/*.txt'].map{|x| [x.split(?/)[-1][0..-5], File.read(x)]}.to_h
-@spec = File.readlines('data/spec').map{|x| x.split(nil, 2)}.to_h.transform_values{|v|
+$VERSION = 0
+$data = Dir['data/*/*.txt'].map{|x| [x.split(?/)[-1][0..-5], File.read(x)]}.to_h
+$spec = File.readlines('data/spec').map{|x| x.split(nil, 2)}.to_h.transform_values{|v|
     if v[0] == ?@
         # bold is inverted here because most colored things should also be bolded
         v[1..-1].split.map{|x| [x[0], [x[1], x[2,2], !x[4]]]}.to_h
@@ -15,8 +11,7 @@ end
         v.split.map{|x| w, d = x.scan(/^\d+|.+/); [w.to_i, d]}
     end
 }
-layout = tr File.read('data/layout-3col').lines.map(&:split)
-colorscheme = '
+$colorschemes = {'dark' => '
         .bk, .C                         { color: #555; }
         .re, .DRA                       { color: #a00; }
         .gr, .VEG, .E                   { color: #0a0; }
@@ -33,8 +28,14 @@ colorscheme = '
         .bm, .CLO, .PLS, .GEM, .MAT     { color: #f5f; }
         .bc, .GLA                       { color: #5ff; }
         .wh, .L, .WAX, .PAP, .BON, .PLT { color: #fff; }
-'
-@colors = colorscheme.scan(/\.(\w+)/).map &:first
+'}
+
+def tr a, c=true
+    transposed = ([nil]*a.map(&:size).max).zip(*a)
+    c ? transposed.map(&:compact) : transposed.map{|x| x[1..-1]}
+end
+
+@colors = $colorschemes.values[0].scan(/\.(\w+)/).map &:first
 def html str, clr, bold=false
     str = (str || '').gsub('&', '&amp;').gsub('<', '&lt;')
     classes = []
@@ -50,9 +51,9 @@ end
 
 def render sec
     rows = [html("##### #{sec} #####".center(80), 'bb'), ' ' * 80]
-    sp = @spec[sec]
-    @data[sec].each_line.with_index do |line, nl|
-        sp = @spec["#{sec}#{nl}"] || sp
+    sp = $spec[sec]
+    $data[sec].each_line.with_index do |line, nl|
+        sp = $spec["#{sec}#{nl}"] || sp
         line.chomp!
         len = 0
         if sp.is_a? Hash
@@ -100,52 +101,58 @@ def render sec
     rows
 end
 
-File.open('cnrs.html', ?w) do |f|
-    f.puts <<~X
-    <!DOCTYPE html>
-    <html lang='en'>
-        <head>
-            <title>cnrs</title>
-            <style>
-            body { background-color: #000; color: #ddd; font: 10px monospace; }
+def go layout, font, bg
+    variant = "html-#{layout}-#{font}-#{bg}-v#{$VERSION}"
+    lyt = tr File.read("data/layout/#{layout}").lines.map(&:split)
+    File.open("out/cnrs-#{variant}.html", ?w) do |f|
+        f.puts <<~X
+        <!DOCTYPE html>
+        <html lang='en'>
+            <head>
+                <title>cnrs</title>
+                <style>
+                body { background-color: #000; color: #ddd; font: #{font}px monospace; }
 
-            #{colorscheme}
-            b { font-weight: normal; }
-            b.fb { font-weight: bold; }
+                #{$colorschemes[bg]}
+                b { font-weight: normal; }
+                b.fb { font-weight: bold; }
 
-            </style>
-        </head>
-        <body>
-            <pre>
-    X
+                </style>
+            </head>
+            <body>
+                <pre>
+        X
 
-    header = ['  ' \
-        "#{html 'comprehensive nethack reference sheet', 'or', true} | " \
-        "<a href='https://github.com/KeyboardFire/cnrs'>https://github.com/KeyboardFire/cnrs</a> | " \
-        "#{html 'andy@keyboardfire.com', 'wh', true} | " \
-        "#{html 'html-3a-10-dark-v0', 'bk'}",
-        '']
+        header = ['  ' \
+            "#{html 'comprehensive nethack reference sheet', 'or', true} | " \
+            "<a href='https://github.com/KeyboardFire/cnrs'>https://github.com/KeyboardFire/cnrs</a> | " \
+            "#{html 'andy@keyboardfire.com', 'wh', true} | " \
+            "#{html variant, 'bk'}",
+            '']
 
-    sep = 'SEP'
-    cols = layout.map do |col|
-        col.map do |sec|
-            case sec
-            when /^\d+$/ then [' '*80] * (sec.to_i-1) + [sep]
-            when ?- then nil
-            else render sec
-            end
-        end.compact.reduce{|a,x| a + [' '*80] + x }
+        sep = 'SEP'
+        cols = lyt.map do |col|
+            col.map do |sec|
+                case sec
+                when /^\d+$/ then [' '*80] * (sec.to_i-1) + [sep]
+                when ?- then nil
+                else render sec
+                end
+            end.compact.reduce{|a,x| a + [' '*80] + x }
+        end
+
+        f.puts header + tr(cols, false).map{|x|
+            x[0] == sep ?
+                html('~' * (82*x.size - 2), 'bc') :
+                x.map{|y| y || ' '*80 } * '  '
+        }
+
+        f.puts <<~X
+                </pre>
+            </body>
+        </html>
+        X
     end
-
-    f.puts header + tr(cols, false).map{|x|
-        x[0] == sep ?
-            html('~' * (82*x.size - 2), 'bc') :
-            x.map{|y| y || ' '*80 } * '  '
-    }
-
-    f.puts <<~X
-            </pre>
-        </body>
-    </html>
-    X
 end
+
+go '3a', '10', 'dark'
